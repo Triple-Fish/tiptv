da451de90afd2ed7bab2c4cd962f72948a591c61
Graham Ward <g.ward@me.com>
2026-09-11 20:16:10 +0100

    Initial Commit

---
+++ IPTVPlayer/IPTVPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  IPTVPlayerApp.swift
+//  IPTVPlayer
+//
+//  Created by Graham Ward on 11/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct IPTVPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
